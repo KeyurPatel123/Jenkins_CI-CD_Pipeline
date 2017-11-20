ef7d4b39e91fa13a43b2c4f04e25db48948b067a
Keyur <keyur.patel@einfochips.com>
2017-11-20 16:18:52 +0530

    Cleanup Updated

--- cleanup.sh
+++ cleanup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-DOCK_IMG=$(docker images -q);
-
-if [[ -z "$DOCK_IMG" ]] ; then
-        echo 'No Docker Images Present'
-else
-        docker rmi -f $DOCK_IMG
-        echo 'Docker Images Removed'
-fi
-
 DOCK_Cont=$(docker ps -a -q);
 
 if [[ -z "$DOCK_Cont" ]] ; then
